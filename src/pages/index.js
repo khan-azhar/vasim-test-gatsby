32b1919ef02d09ad3a2c0c000cec050716244106
khan-azhar <khan.mohammadazhar@gmail.com>
2021-05-23 01:49:25 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,9 @@ const IndexPage = () => (
  
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-   
+    <h1>FlipFlops USA</h1>
+    <p>Welcome to your new Gatsby ecommmerce site.</p>
     
-     <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
     <p>
       //<Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Products Page"</Link>
