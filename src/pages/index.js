f319f25f8af682fa479cec08c3a3bc7b05ef5ca6
khan-azhar <khan.mohammadazhar@gmail.com>
2021-05-22 22:11:34 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,20 +7,22 @@ import Seo from "../components/seo"
 
 const IndexPage = () => (
   const data = this.props.data
+  {data}
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
    
-        {data.allMagentoProduct.edges.map(({ node }) => (
-            <div key={node.id}>
-              <Link to={node.id}>
-                <h2>{node.name}</h2>
-              </Link>
-              <h3>{node.sku}</h3>
-            </div>
-          ))}
+    
+     <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
