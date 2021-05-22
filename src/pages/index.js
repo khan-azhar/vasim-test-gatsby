36e852c59e6a37fb79140dbd079a7e2e7f866275
khan-azhar <khan.mohammadazhar@gmail.com>
2021-05-22 22:20:02 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,7 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
-  const data = this.props.data
-  {data}
+  
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
